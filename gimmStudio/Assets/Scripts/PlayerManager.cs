8ce00ba98453c2d12e39cbe47378eaac5c684a76
Ross <44909473+CrimsonBurster@users.noreply.github.com>
2022-02-27 20:00:24 -0700

    Added simple keyboard player Movement

--- gimmStudio/Assets/Scripts/PlayerManager.cs
+++ gimmStudio/Assets/Scripts/PlayerManager.cs
@@ -6,6 +6,7 @@ using Photon.Pun;
 
 using System;
 using System.Collections;
+using UnityEngine.InputSystem;
 
 namespace Com.MyCompany.MyGame
 {
@@ -15,18 +16,17 @@ namespace Com.MyCompany.MyGame
     /// </summary>
     public class PlayerManager : MonoBehaviourPunCallbacks
     {
-        
+        private float inputX, inputZ;
+        public Rigidbody theRB;
+        public float moveSpeed;
+
         [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
         public static GameObject LocalPlayerInstance;
-
-       
         /// <summary>
         /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
         /// </summary>
         void Awake()
         {
-
-
             // #Important
             // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
             if (photonView.IsMine)
@@ -36,6 +36,24 @@ namespace Com.MyCompany.MyGame
             // #Critical
             // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
             DontDestroyOnLoad(this.gameObject);
+        }
+
+        private void FixedUpdate()
+        {
+            if (photonView.IsMine)
+            {
+                theRB.velocity = new Vector3(inputX * moveSpeed, theRB.velocity.y, inputZ * moveSpeed);
+            }
+        }
+
+        public void Move(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                inputX = context.ReadValue<Vector2>().x;
+
+                inputZ = context.ReadValue<Vector2>().y;
+            }
         }
     }
 }
